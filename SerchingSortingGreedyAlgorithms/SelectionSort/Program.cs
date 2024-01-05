be05822ad487046f2cab51539cc0491430703b99
NikolayGatev <nikolaygatev@gmail.com>
2024-01-05 16:36:55 +0200

    Search&Sorth

---
+++ SerchingSortingGreedyAlgorithms/SelectionSort/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SelectionSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SelectionSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void SelectionSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var min = i;
+
+                for(int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(numbers, i, min);
+            }
+        }
+
+        private static void Swap(int[] numbers, int i, int min)
+        {
+            (numbers[i], numbers[min]) = (numbers[min], numbers[i]);
+        }
+    }
+}
