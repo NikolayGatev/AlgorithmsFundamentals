f8f769cd42ed9e7cf9ea148a526c3057be9afce7
NikolayGatev <nikolaygatev@gmail.com>
2024-01-08 19:01:10 +0200

    Graphs

---
+++ GraphTraversalShortestPaths/ConectedComponents/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.Design;
+
+namespace ConectedComponents
+{
+    public class Program
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+
+       public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            graph = new List<int>[n];
+            visited = new bool[n];
+
+            for (int node = 0; node < n; node++)
+            {
+                var line = Console.ReadLine();
+                
+                if (string.IsNullOrEmpty(line))
+                {
+                    graph[node] = new List<int>();
+                }
+                else
+                {
+                    var children = line.Split(' ').Select(int.Parse).ToList();
+
+                    graph[node] = children;
+                }
+            }
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                if (visited[node])
+                {
+                    continue;
+                }
+
+                var component = new List<int>();
+                DFS(node, component);
+
+                Console.WriteLine(String.Join(" ", component));
+            }
+            
+        }
+
+        private static void DFS(int node, List<int> component)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                DFS(child, component);
+            }
+                component.Add(node);
+        }
+    }
+}
---
+++ GraphTraversalShortestPaths/DFSBFS/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+namespace DFSBFS
+{
+    public class Program
+    {
+        private static Dictionary<int, List<int>> graph;
+        private static HashSet<int> visited;
+
+        public static void Main(string[] args)
+        {
+            graph = new Dictionary<int, List<int>>()
+            {
+                {1, new List<int> {19, 21, 14} },
+                {19, new List<int> {7, 12, 31, 21} },
+                {21, new List<int> {14} },
+                {14, new List<int> {23, 6}},
+                {23, new List<int> {21} },
+                {31, new List<int> {21}},
+                {7, new List<int> {1}},
+                {12, new List<int> ()},
+                {6, new List<int> ()},
+            };
+
+            visited = new HashSet<int>();
+
+            foreach (int node in graph.Keys)
+            {
+                BFS(node);
+            }
+
+            Console.WriteLine();
+
+            visited.Clear();
+            foreach (var node in graph.Keys)
+            {
+                DFS(node);
+            }
+        }
+
+        private static void BFS(int startNode)
+        {
+            if(visited.Contains(startNode))
+            {
+                return;
+            }
+
+            var queue = new Queue<int>();
+            queue.Enqueue(startNode);
+            visited.Add(startNode);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                Console.WriteLine(node);
+
+                foreach ( var child in graph[node])
+                {
+                    if(!visited.Contains(child))
+                    {
+                        visited.Add(child);
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+
+
+        }
+
+        private static void DFS(int node)
+        {
+            if(visited.Contains(node))
+            {
+                return;
+            }
+
+            visited.Add(node);
+
+            foreach (var child in graph[node])
+            {
+                DFS(child);
+            }
+
+            Console.WriteLine(node);
+        }
+    }
+}
