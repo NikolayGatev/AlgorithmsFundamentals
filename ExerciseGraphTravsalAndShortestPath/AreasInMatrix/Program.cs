c470b2efd5ad9e50c8a43998cdf251e190129c60
NikolayGatev <nikolaygatev@gmail.com>
2024-01-10 16:54:53 +0200

    EserciseGraphs

---
+++ ExerciseGraphTravsalAndShortestPath/AreasInMatrix/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+namespace AreasInMatrix
+{
+    public class Program
+    {
+        private static char[,] graph;
+        private static bool[,] visited;
+        private static Dictionary<char, int> areas;
+
+        public static void Main(string[] args)
+        {
+            var row = int.Parse(Console.ReadLine());
+            var col = int.Parse(Console.ReadLine());
+
+            graph = new char[row, col];
+            areas = new Dictionary<char, int>();
+
+            for (int i = 0; i < row; i++)
+            {
+                var rowElements = Console.ReadLine();
+
+                for (int j = 0; j < col; j++)
+                {
+                    graph[i, j] = rowElements[j];
+                }
+            }
+
+            visited = new bool[row, col];
+
+            for(int i = 0;i < row; i++)
+            {
+                for (int j = 0;j < col; j++)
+                {
+                    if (visited[i, j])
+                    {
+                        continue;
+                    }
+                    var nodeValue = graph[i, j];
+                    DFS(i, j, nodeValue);
+
+                    if(areas.ContainsKey(nodeValue))
+                    {
+                        areas[nodeValue]++;
+                    }
+                    else
+                    {
+                        areas[nodeValue] = 1;
+                    }
+                }
+            }
+
+            foreach (var area in areas)
+            {
+                Console.WriteLine($"Letter '{area.Key}' -> '{area.Value}'");
+            }
+
+        }
+
+        private static void DFS(int row, int col, char nodeValue)
+        {
+            if(row < 0 || row >= graph.GetLength(0) ||
+                col < 0 || col >= graph.GetLength(1))
+            {
+                return;
+            }
+
+            if (visited[row, col])
+            {
+                return;
+            }
+
+            if (graph[row, col] != nodeValue)
+            {
+                return;
+            }
+
+            visited[row, col] = true;
+
+            DFS(row, col - 1, nodeValue);
+            DFS(row, col + 1, nodeValue);
+            DFS(row - 1, col, nodeValue);
+            DFS(row + 1, col, nodeValue);
+
+        }
+    }
+}
---
+++ ExerciseGraphTravsalAndShortestPath/DistanceBetweenNodes/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Security.Cryptography;
+
+namespace DistanceBetweenNodes
+{
+    public class Program
+    {
+        private static Dictionary<int, List<int>> graphs;
+        public static void Main(string[] args)
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var pairs = int.Parse(Console.ReadLine());
+
+            graphs = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < nodes; i++)
+            {
+                var nodeAndChildren = Console.ReadLine().Split(":", StringSplitOptions.RemoveEmptyEntries);
+
+                var node = int.Parse(nodeAndChildren[0]);
+
+                if (nodeAndChildren.Length == 1)
+                {
+                    graphs[node] = new List<int>();
+                }
+                else
+                {
+                    var children = nodeAndChildren[1]
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToList();
+                    graphs[node] = children;
+                }
+            }
+            
+            for (int i = 0;i < pairs; i++)
+            {
+                var pair = Console.ReadLine()
+                    .Split("-")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var start = pair[0];
+                var destination = pair[1];
+
+                var steps = BFS(start, destination);
+
+                Console.WriteLine(steps);
+            }
+
+
+        }
+
+        private static int BFS(int start, int destination)
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(start);
+
+            var visited = new HashSet<int>() { start };
+            var parent = new Dictionary<int, int> { { start, -1 } };
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if(node == destination)
+                {
+                    return GetSteps(parent, destination);
+                }
+
+                foreach ( var child in graphs[node])
+                {
+                    if(visited.Contains(child))
+                    {
+                        continue;
+                    }
+
+                    visited.Add(child);
+
+                    queue.Enqueue(child);
+
+                    parent[child] = node;
+                }
+            }
+
+            return -1;
+        }
+
+        private static int GetSteps(Dictionary<int, int> parent, int destination)
+        {
+            var steps = 0;
+            var node = destination;
+
+            while(node != -1)
+            {
+                node = parent[node];
+                steps++;
+            }
+            return steps - 1;
+        }
+    }
+}
